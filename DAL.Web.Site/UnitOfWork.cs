cbfe41e68045652fc3d86d836ab2e2b7b65b8171
Vasily <zilmmz554@yandex.ua>
2017-12-18 19:23:39 +0300

    UnitOfWork, BL

---
+++ DAL.Web.Site/UnitOfWork.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL.Web.Site.Interfaces;
+using DAL.Web.Site.EF;
+using DAL.Web.Site.Models;
+using DAL.Web.Site.Repositories;
+namespace DAL.Web.Site
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+        
+        private SiteContext site_db = new SiteContext();
+        private bool disposed = false;
+        private NewsRepository _newsRepository;
+        private PhotoAlbumRepository _photoRepository;
+        private AnnounceRepository _announceRepository;
+
+        public IRepository<News> News
+        {
+            get
+            {
+                if (_newsRepository == null)
+                    _newsRepository = new NewsRepository(site_db);
+                return _newsRepository;
+            }
+        }
+
+        public IRepository<PhotoAlbum> PhotoAlbums
+        {
+            get
+            {
+                if (_photoRepository == null)
+                    _photoRepository = new PhotoAlbumRepository(site_db);
+                return _photoRepository;
+            }
+        }
+
+        public IRepository<Announce> Announces
+        {
+            get
+            {
+                if (_announceRepository == null)
+                    _announceRepository = new AnnounceRepository(site_db);
+                return _announceRepository;
+            }
+        }
+
+        public void Save()
+        {
+            site_db.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    site_db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ DAL/Interfaces/IUnitOfWorkAdmin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.Models;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    interface IUnitOfWorkAdmin 
+    {
+        IRepository<Course> Courses { get; }
+        IRepository<Degree> Degrees { get; }
+        IRepository<Graduate> Graduates { get; }
+        IRepository<Group> Groups { get; }
+        void Save();
+        void Dispose();
+    }
+}
